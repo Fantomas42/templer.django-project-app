e88c20a08f489f53d604fd87d1b122eb20cf23e9
Fantomas42 <fantomas42@gmail.com>
2012-07-23 15:23:44 +0200

    backporting old version of django-project-application

--- src/templer/django_project_app/__init__.py
+++ src/templer/django_project_app/__init__.py
@@ -1,2 +1,18 @@
 """templer.django_project_app"""
-# TODO: code
+import os
+
+from templer.core.base import BaseTemplate
+
+
+class DjangoProjectApp(BaseTemplate):
+    _template_dir = 'templates/django_project_app'
+    summary = "A basic Django project application skeleton"
+    category = 'Django'
+    vars = []
+    use_cheetah = True
+    help = """
+This creates a basic skeleton for a Django application within a project.
+"""
+
+    def pre(self, command, output_dir, vars):
+        vars['project'] = os.path.split(os.getcwd())[-1]
